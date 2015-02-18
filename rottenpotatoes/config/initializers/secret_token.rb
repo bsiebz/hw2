fa2d2fcdb2966dd5dc4c301ce0cfc494729539fb
bsiebz <bryan.j.sieber@gmail.com>
2015-02-17 23:14:58 -0400

    First commit of rails app

---
+++ rottenpotatoes/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rottenpotatoes::Application.config.secret_token = 'cfb8376adfdbc61ad6e1e51da40fc7a6c0a8958d814be7d475bbfb18b12aa3d4071391343d3f6772596eea2ffaf3dfec1a3dc1d20f50d1407b3ad9f9db6658ae'
