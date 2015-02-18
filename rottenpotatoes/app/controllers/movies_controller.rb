39ce4c68b162f58f9957e00a817d5811ab71438d
Bryan Sieber <bryan_sieber@berkeley.edu>
2015-02-17 22:02:59 -0800

    Time is flying7

--- rottenpotatoes/app/controllers/movies_controller.rb
+++ rottenpotatoes/app/controllers/movies_controller.rb
@@ -10,31 +10,17 @@ class MoviesController < ApplicationController
     @all_ratings = Movie.all(:select => :rating).map(&:rating).uniq
     @ratings = params[:ratings]
 
-    @movies = Movie.all
-    if @ratings != nil
-      keys = @ratings.keys
-      @movies = Movies.where(:rating => keys)
-      if params[:sort_by_title] == 'true'
-        @movies = Movie.find(:all, :order => 'title').where(:rating => keys)
-      elsif params[:sort_by_date] == 'true'
-        @movies = Movie.find(:all, :order => 'release_date').where(:rating => keys)
-      end
-      return @movies
+    if params[:sort_by_title] == 'true'
+      @movies = Movie.find(:all, :order => 'title')
+    elsif params[:sort_by_date] == 'true'
+      @movies = Movie.find(:all, :order => 'release_date')
     else
-      if params[:sort_by_title] == 'true'
-        @movies = Movie.find(:all, :order => 'title')
-      elsif params[:sort_by_date] == 'true'
-        @movies = Movie.find(:all, :order => 'release_date')
-      else
-        @movies = Movie.all
+      @movies = Movie.all
     end
-    return @movies
   end
 
   def new
     # default: render 'new' template
-    @all_ratings = Movie.all_ratings
-    redirect_to movies_path
   end
 
   def create
